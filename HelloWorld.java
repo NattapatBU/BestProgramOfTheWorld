81094fadff09151b49a17e15edd5db4caf9ab5c1
NattapatBU <nattapat.thon@bumail.net>
2025-09-05 13:28:50 +0700

    StartProgram
    flsdkf;lsjgsdjg;wdsfksjh;wle

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("CS318_227G");
+        System.out.println("No.13");
+        System.out.println("Hello World");
+    }
+}
